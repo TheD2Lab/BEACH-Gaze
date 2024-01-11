47fcb439d9cc42912679406cf383cc0f1aeaa067
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-01-10 17:25:01 -0800

    Parameters and FileHandler JSON support started

--- src/analysis/FileHandler.java
+++ src/analysis/FileHandler.java
@@ -1,10 +1,49 @@
 package analysis;
 
+
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter; 
+import java.util.LinkedHashMap; 
+import java.util.HashMap; 
+import org.json.*;
+
 /*
  * Read and write xml, csv, txt files
  * Save into memory
  */
 
 public class FileHandler {
+    static public boolean SaveParametersAsJSON(Parameters parameters,String saveLocation) {
+        try {
+            PrintWriter pw = new PrintWriter(saveLocation);
+            pw.write(buildJSON(parameters).toString()); //Save data as JSON to file
 
+            pw.flush();
+            pw.close();
+
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+
+    static private JSONObject buildJSON(Parameters parameters) { //Builds a JSON object from the Parameters
+        try {
+            JSONObject j = new JSONObject();        
+            j.put("InputDirectory", parameters.inputDirectory);
+            j.put("OutputDirectory", parameters.outputDirectory);
+
+            return j;
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }        
+    }
+
+    /*
+     * Needs ability to read files still
+     * Potentially make FileHandler specifically for Parameters only? If not, buildJSON might be better placed in the Parameters object
+     */
 }
--- src/analysis/Parameters.java
+++ src/analysis/Parameters.java
@@ -7,8 +7,8 @@ public class Parameters {
      * Format of a config file
      * UI and QuickStart creates Parameters object
      */
-    private String inputDirectory;
-    private String outputDirectory;
+    public String inputDirectory;
+    public String outputDirectory;
 
 
     public Parameters(String inputDirectory, String outputDirectory) { //Constructor with variables as the parameters
@@ -17,14 +17,12 @@ public class Parameters {
     }
 
     public Parameters(File parameters) { //Constructor with a file containing the parameters
-
+        /*
+         * Still needs code
+         */
     }
 
-    public String getInputDirectory() {
-        return inputDirectory;
-    }
-    
-    public String getOutputDirectory() {
-        return outputDirectory;
+    public void SaveToJSON(String saveLocation) {
+        FileHandler.SaveParametersAsJSON(this, saveLocation);
     }
 }
