7e7c66aed5c7c849c37bc71f50c9a7d1c4ee7db7
Immersor-git <62318692+Immersor-git@users.noreply.github.com>
2024-01-29 16:11:32 -0800

    Updated Analysis and DataEntry, Implemented DescriptiveStats, Started on Fixation analysis.

--- src/analysis/Fixations.java
+++ src/analysis/Fixations.java
@@ -1,5 +1,30 @@
 package analysis;
 
+import java.util.ArrayList;
+
 public class Fixations {
-    
+    private ArrayList<Double> allDurations;
+    private DataEntry data;
+
+    public Fixations(DataEntry data) {
+        this.data = data;
+        this.allDurations = new ArrayList<Double>();
+    }
+
+    /*
+     *      int fixationDurationIndex = list.indexOf("FPOGD");
+            int fixationIDIndex = list.indexOf("FPOGID");
+            int fixationXIndex = list.indexOf("FPOGX");
+            int fixationYIndex = list.indexOf("FPOGY");
+            int aoiIndex = list.indexOf("AOI");
+            int timestampIndex = list.indexOf("FPOGS");
+     */
+    public void update() {
+        double fixationDuration = Double.valueOf(data.getValue("FPOGD"));
+        allDurations.add(fixationDuration);
+    }
+
+    public void process() {
+        
+    }
 }
