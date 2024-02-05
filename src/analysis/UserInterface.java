8aa6d5d50b9bc123b681693c2c7598dfbe48804a
angeloryan <angeloryansoriano@gmail.com>
2024-02-05 14:38:37 -0800

    Added gaze analysis panel to UI

--- src/analysis/UserInterface.java
+++ src/analysis/UserInterface.java
@@ -6,80 +6,89 @@ import java.util.HashMap;
 
 
 public class UserInterface {
-    
+
     private JFrame frame;
-    private HashMap<String, Integer> windowSettings;
-    private String[] inputFiles;
-    private String outputDirectory;
-    
+
     public UserInterface() {
-        initComponents();
-        buildFrame();
-        setEventHandling();
-    }
-
-    private void initComponents() {
-        frame = new JFrame("Gazepoint Data Analysis");
-        frame.setLayout(new GridBagLayout());
-
-        // 
-        JPanel analysisPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints analysisPanelConstraints = new GridBagConstraints();
-        
-        analysisPanel.setBackground(Color.yellow);
-
-        JCheckBox batchAnalysisCheckBox = new JCheckBox("Batch Analysis");
-        JButton selectFilesButton = new JButton("Select Files");
-        GridBagConstraints analysisComponentConstraints = new GridBagConstraints();
-
-        analysisPanel.add(batchAnalysisCheckBox, analysisComponentConstraints);
-        analysisPanel.add(selectFilesButton, analysisComponentConstraints);
-
-        frame.add(analysisPanel, analysisPanelConstraints);
-
-        // 
-        JPanel windowPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints windowConstraints = new GridBagConstraints();
-        JCheckBox continuousCheckBox = new JCheckBox("Continuous");
-        JCheckBox cumulativeCheckBox = new JCheckBox("Cumulative");
-        JCheckBox overlappingCheckBox = new JCheckBox("Overlapping");
-        JCheckBox eventCheckBox = new JCheckBox("Event");
-
-        windowPanel.add(continuousCheckBox, windowConstraints);
-        windowPanel.add(cumulativeCheckBox, windowConstraints);
-        windowPanel.add(overlappingCheckBox, windowConstraints);
-        windowPanel.add(eventCheckBox, windowConstraints);
-
-        frame.add(windowPanel);
-
-        // 
-        JPanel consolePanel = new JPanel(new GridBagLayout());
-        GridBagConstraints consoleConstraints = new GridBagConstraints();
-        JButton runAnalysisButton = new JButton("Run Analysis");
-
-        consolePanel.add(runAnalysisButton, consoleConstraints);
-        
-        frame.add(consolePanel);
-
-        // 
-        JPanel contactInfoPanel = new JPanel(new GridBagLayout());
-        GridBagConstraints contactInfoConstraints = new GridBagConstraints();
-        
-        frame.add(contactInfoPanel);
-    }
-
-    private void buildFrame() {
+        // Initialize UI frame
+        frame = new JFrame("Gazepoint Analysis Tool");
         frame.setSize(800, 600);
         frame.setResizable(false);
+        frame.setLayout(new GridBagLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Constraints dictate location of UI components
+        GridBagConstraints panelGBC = new GridBagConstraints(); 
+        GridBagConstraints componentGBC = new GridBagConstraints();
+
+        // Panel contains all components pertaining to gaze settings
+        JPanel gazePanel = new JPanel();
+        gazePanel.setSize(400, 300);
+        gazePanel.setLayout(new GridBagLayout());
+        panelGBC.anchor = GridBagConstraints.FIRST_LINE_START;
+        panelGBC.gridx = 0;
+        panelGBC.gridy = 0;
+        panelGBC.gridwidth = 1;
+        panelGBC.gridheight = 1;
+        panelGBC.weightx = 1;
+        panelGBC.weighty = 1;
+
+        JLabel gazeLabel = new JLabel("Gaze Analysis Settings");
+        componentGBC.anchor = GridBagConstraints.FIRST_LINE_START;
+        componentGBC.ipadx = 10;
+        componentGBC.ipady = 10;
+        componentGBC.gridx = 0;
+        componentGBC.gridy = 0;
+        componentGBC.gridwidth = GridBagConstraints.REMAINDER;
+        componentGBC.gridheight = 1;
+        gazePanel.add(gazeLabel, componentGBC);
+
+        JButton selectFiles = new JButton("Select File(s)");
+        componentGBC.anchor = GridBagConstraints.FIRST_LINE_START;
+        componentGBC.gridx = 0;
+        componentGBC.gridy = 1;
+        componentGBC.gridwidth = 1;
+        componentGBC.gridheight = 1;
+        gazePanel.add(selectFiles, componentGBC);
+
+        JLabel fileCount = new JLabel("# files selected");
+        componentGBC.gridx = 0;
+        componentGBC.gridy = 2;
+        componentGBC.gridwidth = 1;
+        componentGBC.gridheight = 1;
+        gazePanel.add(fileCount, componentGBC);
+
+        JLabel directoryLabel = new JLabel("Output Directory");
+        componentGBC.gridx = 0;
+        componentGBC.gridy = 3;
+        componentGBC.gridwidth = 1;
+        componentGBC.gridheight = 1;
+        gazePanel.add(directoryLabel, componentGBC);
+
+        JButton button2 = new JButton("Select Directory");
+        componentGBC.gridx = 0;
+        componentGBC.gridy = 4;
+        componentGBC.gridwidth = 1;
+        componentGBC.gridheight = 1;
+        gazePanel.add(button2, componentGBC);
+
+        frame.add(gazePanel, panelGBC);
+
+        // Wipe constraints
+        panelGBC = new GridBagConstraints(); 
+        componentGBC = new GridBagConstraints();
+
+        // Panel contains all components pertaining to window analysis
+        JPanel windowsPanel = new JPanel();
+        windowsPanel.setLayout(new GridBagLayout());
+        //panelGBC.anchor = GridBagConstraints.FIRST_LINE_START;
+        panelGBC.gridx = 1;
+        panelGBC.gridy = 0;
+        panelGBC.gridwidth = 1;
+        panelGBC.gridheight = 1;
+
+        frame.add(windowsPanel, panelGBC);
+
         frame.setVisible(true);
-    }
-
-    private void setEventHandling() {
-
-    }
-
-    public Parameters getParameters() {
-        return new Parameters(inputFiles, outputDirectory, windowSettings);
     }
 }
