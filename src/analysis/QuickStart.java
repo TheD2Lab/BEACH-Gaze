c1ad40fb9964ab668cd8e32702d6304a806dc8c6
angeloryan <angeloryansoriano@gmail.com>
2024-02-13 10:25:16 -0800

    Update QuickStart.java

--- src/analysis/QuickStart.java
+++ src/analysis/QuickStart.java
@@ -1,12 +1,14 @@
 package analysis;
 
+import java.io.File;
+
 public class QuickStart {
     // configDirectory should be initialized to a file path containing the config file
     static final String configDirectory = "";
 
     public static void main(String[] args) {
-        // Parameters params = new Parameters(new String[]{"data//Esther Jung_all_gaze.csv"}, "C://Users//Productivity//Documents//Testing//OutputTest.csv",new WindowAnalysis());
-        // Analysis analysis = new Analysis(params);
-        // analysis.run();
+        Parameters params = new Parameters(new File[]{new File("data//Esther Jung_all_gaze.csv")}, "C://Users//Angelo//Documents//Testing//OutputTest.csv",new WindowAnalysis());
+        Analysis analysis = new Analysis(params);
+        analysis.run();
     }
 }
