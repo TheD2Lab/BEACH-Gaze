4dbbd3a32ce2eef7cf9b632c01a5e2311f5554cc
ashkjones <ashkjones@gmail.com>
2024-02-22 17:06:01 -0800

    Create cleaning method
    New filter checks that pupil size is humanly possible and checks that gaze entry occurred within the bounds of the monitor (i.e. person was looking a the screen). Also capitalized the file names to match the class name they contain for case sensitive operating systems (i.e. UNIX based ones)

--- src/analysis/DataFiltering.java
+++ src/analysis/DataFiltering.java
@@ -30,6 +30,55 @@ public class DataFiltering {
         return filtered;
     }
 
+    /**
+     * Cleanses data by filtering out invalid data. Valid data entries must occur within
+     * the bounds of the monitor and have humanly possible pupil dilation.
+     * @param data Data to be cleansed
+     */
+    public static DataEntry filterByValidity(DataEntry data) {
+        // humanly possible pupil diameter is between 2 and 8 mm
+        final int MIN_DIAMETER = 2;
+        final int MAX_DIAMETER = 8;
+        final int MAX_PUPIL_DIFF = 1;   
+        // GazePoint scales point of gaze location from 0 to 1 when on screen
+        final int MIN_SCREEN_DIM = 0;   
+        final int MAX_SCREEN_DIM = 1;   
+
+        DataEntry filtered = new DataEntry(data.getHeaders());
+        for (int rowNum = 0; rowNum < data.rowCount(); rowNum++) {
+            // Note: It is extremely slow to parse a string over and over again
+            // Check if Gazepoint could detect the pupils
+            Boolean leftValid = Integer.parseInt(data.getValue("LPMMV", rowNum)) == 1;
+            Boolean rightValid = Integer.parseInt(data.getValue("RPMMV", rowNum)) == 1;
+            if (!(leftValid && rightValid)) {
+                continue;   // skip invalid entry
+            } 
+            // Check if POG is on the screen
+            float xCoordinate = Float.parseFloat(data.getValue("FPOGX" ,rowNum));
+            float yCoordinate = Float.parseFloat(data.getValue("FPOGY" ,rowNum));
+            if (xCoordinate < MIN_SCREEN_DIM || xCoordinate > MAX_SCREEN_DIM) {
+                continue; // off screen in x-direction, invalid
+            } else if (yCoordinate < MIN_SCREEN_DIM || yCoordinate > MAX_SCREEN_DIM) {
+                continue; // off screen in y-direction, invalid entry
+            }
+            // Check if pupils are valid sizes individually and compared to each other.
+            float leftDiameter = Float.parseFloat(data.getValue("LPMM", rowNum));
+            float rightDiameter = Float.parseFloat(data.getValue("RPMM", rowNum));
+            if (
+                leftDiameter >= MIN_DIAMETER
+                && leftDiameter <= MAX_DIAMETER
+                && rightDiameter >= MIN_DIAMETER
+                && rightDiameter <= MAX_DIAMETER
+                && (Math.abs(leftDiameter-rightDiameter) <= MAX_PUPIL_DIFF)
+            ) {
+                filtered.process(data.getRow(rowNum));
+                continue;
+            }
+        }
+        return filtered;
+    }
+
+
     static public LinkedHashMap<String, Data> filterByAOI(DataEntry data ){
         
         return new LinkedHashMap<String, Data>();
